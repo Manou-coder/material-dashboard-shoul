97d430a90661208fcc3f18ba7d2eaf5262440342
Manou <manouhaouzi@gmail.com>
2023-05-31 12:47:17 +0300

    fix dashboard nav component

--- src/pages/dashboard/zmanim.tsx
+++ src/pages/dashboard/zmanim.tsx
@@ -1,7 +1,34 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+} from '@material-tailwind/react'
+
 export const Zmanim = () => {
   return (
-    <section className="">
-      <h1>hello world</h1>
+    <section className="mt-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div>
+          <ZmanimCard />
+        </div>
+        <div>
+          <ZmanimCard />
+        </div>
+        <div>
+          <ZmanimCard />
+        </div>
+      </div>
     </section>
+  )
+}
+
+const ZmanimCard = () => {
+  return (
+    <Card>
+      <CardBody>
+        <h1>hello world</h1>
+      </CardBody>
+    </Card>
   )
 }
